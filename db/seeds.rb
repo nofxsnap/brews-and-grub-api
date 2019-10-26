06a97f15a77df28020a24c560f44a4559e21a08e
John Siladie <john.siladie@travelershaven.com>
2019-10-26 08:50:49 -0600

    Add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Brewery.create!(name: 'Denver Beer Co', url: 'http://denverbeerco.com/on-tap-today/')
